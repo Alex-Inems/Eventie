05055afa9326352e60deb58fe2654794d5368323
Alex <inema9886@gmail.com>
2025-03-13 19:21:58 +0100

    fix csp issue

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const securityHeaders = [
     key: 'Content-Security-Policy',
     value: `
       default-src 'self';
-      script-src 'self' 'unsafe-inline' https://checkout.paystack.com https://www.googletagmanager.com https://www.gstatic.com https://www.googleapis.com;
+      script-src-elem 'self' 'unsafe-inline' https://checkout.paystack.com https://www.googletagmanager.com https://www.gstatic.com https://www.googleapis.com;
       frame-src 'self' https://checkout.paystack.com https://firebasestorage.googleapis.com;
       connect-src 'self' https://api.paystack.co https://firestore.googleapis.com https://www.googleapis.com;
       img-src 'self' https://firebasestorage.googleapis.com https://lh3.googleusercontent.com https://images.unsplash.com data:;
@@ -13,7 +13,7 @@ const securityHeaders = [
       font-src 'self' https://fonts.gstatic.com;
     `.replace(/\n/g, '') // Remove all line breaks
   }
-]
+];
 
 const nextConfig: NextConfig = {
   images: {
